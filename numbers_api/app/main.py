2670e7062d71c4af71af37041c40516a4b7c0a35
Roy Tamir <roytamir@gmail.com>
2024-03-13 23:52:10 +0200

    fixes

--- numbers_api/app/main.py
+++ numbers_api/app/main.py
@@ -9,7 +9,7 @@ def random_odd():
 
 @app.get("/even")
 def random_even():
-    return random.choice([num for num in range(1, 100) if num % 2 == 0])
+    return random.choice([num for num in range(1, 20) if num % 2 == 0])
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=80)
